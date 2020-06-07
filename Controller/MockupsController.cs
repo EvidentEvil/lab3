d5eed0442f48bd4d59bf829731ba82de4f7dc8ac
EvidentLizard <horomi.eien@mail.ru>
2020-06-07 23:52:52 +0700

    lab3 @Varabei

---
+++ Controller/MockupsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Backend3.Models;
+
+namespace Backend3.Controllers
+{
+    public class MockupsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Quiz()
+        {
+            Operations operations = new Operations();
+            return View(operations);
+        }
+
+        [HttpPost]
+        public IActionResult Quiz(Operations operation, string action)
+        {
+            double number;
+            if (double.TryParse(operation.YourAnswer, out number) & ModelState.IsValid)
+            {
+                CorrectAnswers answers = CorrectAnswers.Instance;
+                answers.Total+= 1;
+                operation.Solution(); 
+                ViewData["NotANumber"] = "" + operation.First + 
+                    "  ||  " + operation.Second;
+                if (operation.Check())
+                    answers.Correct += 1;
+                (answers.Answers).Add(operation);
+            }
+            else
+            {
+                ViewData["NotANumber"] = "Not a number!";
+                return View(operation);
+            }
+
+            if (action == "Next")
+                return View(new Operations());
+            return RedirectToAction("QuizResult");
+        }
+
+        public IActionResult QuizResult()
+        {
+            CorrectAnswers answers = CorrectAnswers.Instance;
+            ViewBag.Result = answers.Answers;
+            ViewData["Correct"] = "" + answers.Correct;
+            ViewData["Total"] = "" + answers.Total;
+            return View();
+        }
+    }
+}
